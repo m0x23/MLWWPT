c53995f63dbfc327ca8460df1ecb634a1ceb01a0
Mark Breukelman <mark.breukelman@connox.de>
2017-03-29 15:21:37 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <div id="footer"></div><!-- footer -->
-<a id="922916" href="http://www.besucherzaehler-homepage.de">www.besucherzaehler-homepage.de</a><script type="text/javascript" language="JavaScript" src="https://www.besucherzaehler-homepage.com/counter_js.php?account=922916&style=29"></script>	
  
 </div><!-- wrapper -->
 	<?php wp_footer(); ?>
